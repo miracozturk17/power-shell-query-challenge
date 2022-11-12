7985eac5b9eac61b167e47917934dde4ade128ad
Miraç  ÖZTÜRK <32125234+miracozturk17@users.noreply.github.com>
2022-11-12 19:19:47 +0300

    Users
    Users

---
+++ Users.sql
@@ -0,0 +1,16 @@
+USE [PowerBIServiceLog]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 12.11.2022 19:19:26 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[UserIdentifier] [nvarchar](250) NULL
+) ON [PRIMARY]
+GO
+
+
